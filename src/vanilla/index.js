7b45c4a82e4f7ff27fd89cedf300228d9c9ca317
Kiyoshi09 <8742202+Kiyoshi09@users.noreply.github.com>
2023-10-14 23:18:48 +0900

    access token

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -1,3 +1,4 @@
+import { access } from 'fs';
 import './index.css';
 import liff from '@line/liff';
 
@@ -6,6 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
     .init({ liffId: '2001156183-2w0837lY' })
     .then(() => {
       console.log(`=== Success! you can do something with LIFF API here ===`);
+
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      } else {
+        const accessToken = liff.getAccessToken();
+        console.log(`Access Token : ${accessToken}`);
+      }
     })
     .catch((error) => {
       console.log(error);
