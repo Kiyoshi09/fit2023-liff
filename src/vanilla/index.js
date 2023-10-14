63730e59780818c956c901617eec4567634a5f83
Kiyoshi09 <8742202+Kiyoshi09@users.noreply.github.com>
2023-10-14 22:46:30 +0900

    added liffid

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -3,9 +3,9 @@ import liff from '@line/liff';
 
 document.addEventListener('DOMContentLoaded', function () {
   liff
-    .init({ liffId: process.env.LIFF_ID })
+    .init({ liffId: '2001156183-2w0837lY' })
     .then(() => {
-      console.log(`Success! you can do something with LIFF API here: [${process.env.LIFF_ID}]`);
+      console.log(`=== Success! you can do something with LIFF API here ===`);
     })
     .catch((error) => {
       console.log(error);
